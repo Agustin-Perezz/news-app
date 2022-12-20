59c8d400c05bcbe307e40552792afd9a08b00fbf
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-20 15:56:01 -0300

    Change console message

--- src/services/public.service.ts
+++ src/services/public.service.ts
@@ -6,7 +6,7 @@ export const getNewsByQuery = async <T>(query: string) => {
     const { data } = await newsApi.get<T>(
       `/all?api_token=${process.env.REACT_APP_API_KEY}&search=${query}`
     );
-    console.log('data: ', data);
+    console.log('data-response: ', data);
   } catch (error) {
     console.log(error);
     if (error instanceof Error) {
