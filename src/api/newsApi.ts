e6142cc67462e02c83e6400cca53b553b335d02d
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-20 15:41:58 -0300

    Change api url and key

--- src/api/newsApi.ts
+++ src/api/newsApi.ts
@@ -2,7 +2,4 @@ import axios from 'axios';
 
 export const newsApi = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    'x-api-key': process.env.REACT_APP_API_KEY,
-  },
 });
