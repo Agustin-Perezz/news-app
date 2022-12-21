001cc8408114d6a213f1456b59fb222445d9fbd2
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-21 09:23:50 -0300

    Add CustomStack component

---
+++ src/components/CustomStack/CustomStack.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Stack, { StackProps } from '@mui/material/Stack';
+
+type CustomStackProps = {
+  direction: StackProps['direction'];
+  justifyContent: StackProps['justifyContent'];
+  alignItems: StackProps['alignItems'];
+  marginBottom: number;
+};
+
+type Props = {
+  stackProps: CustomStackProps;
+  children: React.ReactNode;
+};
+
+export const CustomStack: React.FC<Props> = ({ stackProps, children }) => {
+  return <Stack {...stackProps}> {children} </Stack>;
+};
