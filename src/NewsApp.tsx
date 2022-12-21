996300901baddcf61a05f2dec40dfd83777b4b90
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-21 09:36:41 -0300

    Add NavBar component

--- src/NewsApp.tsx
+++ src/NewsApp.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { NavBar } from './components/NavBar/NavBar';
 import { AppTheme } from './theme';
 
 export const NewsApp: React.FC = () => {
   return (
     <AppTheme>
-      <h3>News App Component.</h3>
-      <span> this is a test</span>
+      <NavBar />
     </AppTheme>
   );
 };
