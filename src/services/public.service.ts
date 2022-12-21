500b23bd387e72b8c8f6763a75cb270d141c9bc9
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-21 09:13:42 -0300

    Add hook useAxios

--- src/services/public.service.ts
+++ src/services/public.service.ts
@@ -1,12 +1,17 @@
 import { newsApi } from '../api';
+import { loadAbort } from '../utilities';
 
 /* eslint-disable consistent-return */
 export const getNewsByQuery = async <T>(query: string) => {
+  const controller = loadAbort();
   try {
-    const { data } = await newsApi.get<T>(
-      `/all?api_token=${process.env.REACT_APP_API_KEY}&search=${query}`
-    );
-    console.log('data-response: ', data);
+    return {
+      call: newsApi.get<T>(
+        `/all?api_token=${process.env.REACT_APP_API_KEY}&search=${query}`,
+        { signal: controller.signal }
+      ),
+      controller,
+    };
   } catch (error) {
     console.log(error);
     if (error instanceof Error) {
