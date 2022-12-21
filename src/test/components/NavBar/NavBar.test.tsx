66269830cb9f27bdb02210adf649bf715d280454
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-21 15:07:41 -0300

    Set jest with test-library

---
+++ src/test/components/NavBar/NavBar.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { NavBar } from '../../../components/NavBar/NavBar';
+
+describe('Test in <Navbar />', () => {
+  beforeEach(() => {
+    render(<NavBar />);
+  });
+  it('should match whit snapshot', () => {
+    expect(screen).toMatchSnapshot();
+  });
+  it('should contain main icons and titles in navbar', () => {
+    expect(screen.getByText('The power of information')).toBeInTheDocument();
+    expect(screen.getByTestId('GitHubIcon')).toBeInTheDocument();
+    expect(screen.getByTestId('LinkedInIcon')).toBeInTheDocument();
+    expect(screen.getByTestId('TwitterIcon')).toBeInTheDocument();
+  });
+});
