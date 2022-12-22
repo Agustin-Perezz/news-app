02af82df631495fee78f3e59e64746ca503fddd4
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-22 16:23:41 -0300

    Add ListArticles component

---
+++ src/components/Articles/ListArticles.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import { NewsProps } from '../../types/news-props';
+import { Article } from './Article';
+import { RenderController } from '../ui/RenderController';
+
+interface Props {
+  dataArticles?: NewsProps[];
+  isLoading: boolean;
+  isError?: string;
+}
+
+export const ListArticles: React.FC<Props> = ({
+  dataArticles,
+  isLoading,
+  isError,
+}) => {
+  return (
+    <RenderController
+      isLoading={isLoading}
+      isError={isError}
+      data={dataArticles}
+    >
+      <Box sx={{ flexGrow: 1, marginTop: 3 }}>
+        <Typography variant="h5">Results for search:</Typography>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 0, sm: 8, md: 12 }}
+          justifyContent="center"
+        >
+          {dataArticles?.map((article) => (
+            <Grid item xs={2} sm={4} md={3.5} key={article.nameSource}>
+              <Article dataArticle={article} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </RenderController>
+  );
+};
