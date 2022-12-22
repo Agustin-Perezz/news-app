b0388b4c064ca54c655570fa005d4ddce9674611
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-22 17:52:37 -0300

    Add date format in createAddaptNew

--- src/adapters/news.adapter.ts
+++ src/adapters/news.adapter.ts
@@ -4,12 +4,15 @@ export const createAddaptNew = (
   newResponse: NewsResponseProps
 ): NewsProps[] => {
   const listArticles: NewsProps[] = newResponse.data.map((data) => {
+    const dateFormated = new Date(data.published_at).toLocaleDateString(
+      'en-US'
+    );
     return {
       uuid: data.uuid,
       title: data.title,
       description: data.description,
       image_url: data.image_url,
-      published_at: data.published_at,
+      published_at: dateFormated,
       nameSource: data.source,
       categories: data.categories,
     };
