0947283521f314df269f364501404bdf852a069d
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-22 15:45:50 -0300

    Add SocialMedia test

---
+++ src/test/ui/SocialMedia/SocialMedia.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import { SocialMedia } from '../../../components/ui/SocialMedia';
+
+describe('Test in <SocialMedia />', () => {
+  let globalContainer: HTMLElement;
+  beforeEach(() => {
+    const { container } = render(<SocialMedia />);
+    globalContainer = container;
+  });
+
+  it('should match snapshot', () => {
+    expect(screen).toMatchSnapshot();
+  });
+  it('should have 3 html elements <a/> whit respectives href url', () => {
+    const aTags = globalContainer.querySelectorAll('a');
+    expect(aTags.length).toBe(3);
+    expect(aTags[0]).toContainHTML('a');
+    expect(aTags[0]).toHaveAttribute(
+      'href',
+      'https://github.com/Agustin-Perezz'
+    );
+    expect(aTags[1]).toHaveAttribute(
+      'href',
+      'https://www.linkedin.com/in/agustinperez-front-end-developer/'
+    );
+    expect(aTags[2]).toHaveAttribute(
+      'href',
+      'https://twitter.com/agustinperez__'
+    );
+  });
+  it('should contain respectives styles', () => {
+    const avatarClasses =
+      globalContainer.getElementsByClassName('MuiAvatar-root');
+    expect(avatarClasses[0].className).toBe(
+      'MuiAvatar-root MuiAvatar-circular MuiAvatar-colorDefault css-4blva6-MuiAvatar-root'
+    );
+    expect(avatarClasses.length).toBe(3);
+  });
+});
