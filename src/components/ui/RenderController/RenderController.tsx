7286ce919ec6937b3ca176b22fe105aaba70eaaa
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-22 16:07:00 -0300

    Add RenderController component

---
+++ src/components/ui/RenderController/RenderController.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NewsProps } from '../../../types/news-props';
+import { ErrorBox } from '../Error';
+import { LoadingSkeleton } from './LoadingSkeleton';
+
+export type PropsRender = {
+  children: React.ReactNode;
+  isLoading: boolean;
+  isError?: string;
+  data?: NewsProps[];
+};
+
+export const RenderController: React.FC<PropsRender> = ({
+  children,
+  data,
+  isLoading,
+  isError,
+}) => {
+  if (isLoading) return <LoadingSkeleton />;
+
+  if (isError) return <ErrorBox errorMessage={isError} />;
+
+  if (data?.length === 0)
+    return <div>Ups, no results, try with others things.</div>;
+
+  return <div>{children}</div>;
+};
