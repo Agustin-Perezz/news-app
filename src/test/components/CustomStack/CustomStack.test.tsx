1428d3ca31a0aa9ef22d87626308ff6eee4ddf87
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-23 09:25:32 -0300

    Add CustomStack test

---
+++ src/test/components/CustomStack/CustomStack.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { CustomStack } from '../../../components/CustomStack';
+
+describe('Test in <CustomStack />', () => {
+  let globalContainer: HTMLElement;
+  beforeEach(() => {
+    const { container } = render(
+      <CustomStack
+        stackProps={{
+          alignItems: 'center',
+          direction: 'row',
+          justifyContent: 'space-around',
+          marginBottom: 2,
+        }}
+      >
+        <h2> Title example </h2>
+      </CustomStack>
+    );
+    globalContainer = container;
+  });
+
+  it('should match snapshot', () => {
+    expect(screen).toMatchSnapshot();
+  });
+  it('must contain past props', () => {
+    const element = globalContainer.getElementsByClassName(
+      'css-1kkugh-MuiStack-root'
+    );
+    expect(element[0]).toHaveStyle('justify-content: space-around');
+    expect(element[0]).toHaveStyle('align-items: center');
+  });
+  it('should render a children', () => {
+    expect(screen.getByText('Title example')).toBeInTheDocument();
+    expect(screen.getByText('Title example')).toContainHTML('h2');
+  });
+});
