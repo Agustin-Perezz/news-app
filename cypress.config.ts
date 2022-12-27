3648adc9dac1544d2663d449093f2f03e26f9fd7
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-12-27 11:30:10 -0300

    Add cypress test with api call and change cypress.config to accept process.env

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,9 @@
+/* eslint-disable no-param-reassign */
 import { defineConfig } from 'cypress';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
@@ -6,5 +11,16 @@ export default defineConfig({
     supportFile: 'cypress/support/index.ts',
     viewportHeight: 700,
     viewportWidth: 1180,
+  },
+  env: {
+    API_URL: process.env.REACT_APP_API_URL,
+    API_KEY: process.env.REACT_APP_API_KEY,
+  },
+  setupNodeEvents(__, config) {
+    config.env = {
+      ...process.env,
+      ...config.env,
+    };
+    return config;
   },
 });
