db0765332cae43154901b7885e77a518278c602b
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-01-03 17:59:50 -0300

    Add test useAxios and add jest environment variables

--- src/test/api/newsApi.test.ts
+++ src/test/api/newsApi.test.ts
@@ -2,6 +2,6 @@ import { newsApi } from '../../api';
 
 describe('Test in newsApi', () => {
   it('should contian baseURL', () => {
-    expect(newsApi.defaults.baseURL).toBe(process.env.REACT_APP_API_KEY);
+    expect(newsApi.defaults.baseURL).toBe(process.env.REACT_APP_API_URL);
   });
 });
--- src/test/hooks/useAxios.test.ts
+++ src/test/hooks/useAxios.test.ts
@@ -1,17 +1,34 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useAxios } from '../../hooks';
+import { newsApi } from '../../api';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 describe('Test in useAxios', () => {
   function setup() {
-    const {
-      result: { current },
-      rerender,
-    } = renderHook(() => useAxios());
-    return { current, rerender };
+    const results = renderHook(() => useAxios());
+    return results;
   }
 
-  it('should init loading in false', () => {
-    const { current } = setup();
-    expect(current.isLoading).toBe(false);
+  it('should init default values', () => {
+    const { result } = setup();
+    expect(result.current.isLoading).toBe(false);
+  });
+  it('should call fetchData with correctly arguments', async () => {
+    const { result } = setup();
+    result.current.fetchData = jest.fn();
+
+    const parameters = {
+      programApi: newsApi,
+      url: `/all?api_token=${API_KEY}&search=messi`,
+      urlParameter: 'messi',
+    };
+
+    await act(async () => {
+      await result.current.fetchData({ ...parameters });
+    });
+
+    expect(result.current.fetchData).toHaveBeenCalled();
+    expect(result.current.fetchData).toHaveBeenCalledWith({ ...parameters });
   });
 });
