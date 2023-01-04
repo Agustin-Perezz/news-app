8268379c71f1f9921977e0d895960d98dcffe87a
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-01-04 16:48:25 -0300

    Change some styles in ListArticles

--- src/pages/Articles/components/ListArticles.tsx
+++ src/pages/Articles/components/ListArticles.tsx
@@ -22,7 +22,7 @@ export const ListArticles: React.FC<Props> = ({
       data={dataArticles}
     >
       <Box sx={{ flexGrow: 1, my: 3 }}>
-        <Typography variant="h5" mb={3}>
+        <Typography variant="h5" mb={3} textAlign="center" color="snow">
           Results for search:
         </Typography>
         <Grid
@@ -30,6 +30,10 @@ export const ListArticles: React.FC<Props> = ({
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 0, sm: 8, md: 13 }}
           justifyContent="center"
+          sx={{
+            maxWidth: '1200px',
+            margin: '0px auto 20px auto !important',
+          }}
         >
           {dataArticles?.map((article) => (
             <Grid item xs={2} sm={4} md={3.5} key={article.uuid}>
