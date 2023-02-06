1d13255f381fd2e32afd36389bf8e071a8862264
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-02-06 19:35:59 -0300

    Set active option in the cache

--- src/pages/News/hooks/useControllerSort.ts
+++ src/pages/News/hooks/useControllerSort.ts
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import { SelectChangeEvent } from '@mui/material';
 import { useSearch } from '../../Header/hooks';
 import { getSortParameters } from '../utilities';
+import { CacheContext } from '../../../context/CacheContext';
 
 export const useControllerSort = () => {
   const [optionSort, setOptionSort] = React.useState('Most Recent');
+
+  const { cache, setCache } = useContext(CacheContext);
 
   const { search } = useLocation();
   const { handleSubmit } = useSearch();
@@ -14,7 +17,12 @@ export const useControllerSort = () => {
     const { sortParamaters } = getSortParameters(search, value);
     setOptionSort(value);
     handleSubmit({ ...sortParamaters });
+    setCache({ key: 'option-value', value });
   };
+
+  React.useEffect(() => {
+    setOptionSort(cache['option-value'] || 'Most Recent');
+  }, [cache]);
 
   return {
     optionSort,
