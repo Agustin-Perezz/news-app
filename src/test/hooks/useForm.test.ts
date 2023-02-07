17df0e00c67b2376f53fb7f26156e5359d3dc8a4
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-02-07 10:45:29 -0300

    Fix useForm test

--- src/test/hooks/useForm.test.ts
+++ src/test/hooks/useForm.test.ts
@@ -2,20 +2,15 @@ import { renderHook, act } from '@testing-library/react';
 import { useForm } from '../../pages/Header/hooks';
 
 describe('Test in useForm', () => {
-  // function setup(param: string) {
-  //   const result = renderHook(() => useForm(param));
-  //   return result;
-  // }
-  // it('should get initial value from props', () => {
-  //   const { result } = setup('test');
-  //   expect(result.current.query).toBe('test');
-  // });
-  // it('should change te value at call onChange', () => {
-  //   const { result } = setup('');
-  //   expect(result.current.query).toBe('');
-  //   act(() => {
-  //     result.current.onChange('test');
-  //   });
-  //   expect(result.current.query).toBe('test');
-  // });
+  const handleSubmit = jest.fn();
+  // const onChange = jest.fn();
+  function setup() {
+    const result = renderHook(() => useForm({ handleSubmit }));
+    return result;
+  }
+  it('should initializer correctly', () => {
+    const { result } = setup();
+    expect(result.current.onChange).toBeInstanceOf(Function);
+    expect(result.current.onSubmit).toBeInstanceOf(Function);
+  });
 });
