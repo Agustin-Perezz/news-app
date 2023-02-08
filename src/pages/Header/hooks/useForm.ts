e3f9a09726653386d557367845e99d3e9e66d590
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-02-08 19:40:29 -0300

    Clean input value with removePropertyCache function

--- src/pages/Header/hooks/useForm.ts
+++ src/pages/Header/hooks/useForm.ts
@@ -1,10 +1,12 @@
 import React, { useState, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { CacheContext } from '../../../context/CacheContext';
 import { HandleSubmitProps } from '../../../types';
 import { getSortParameters } from '../../../utilities';
 
 export const useForm = ({ handleSubmit }: HandleSubmitProps) => {
-  const { cache, setCache } = useContext(CacheContext);
+  const { cache, setCache, removePropertyCache } = useContext(CacheContext);
+  const { pathname } = useLocation();
 
   const [query, setQuery] = useState(cache['query-value'] || '');
 
@@ -19,6 +21,14 @@ export const useForm = ({ handleSubmit }: HandleSubmitProps) => {
     setCache({ key: 'query-value', value: query });
     handleSubmit({ ...sortParamaters });
   };
+
+  React.useEffect(() => {
+    if (pathname !== '/search') {
+      setQuery('');
+      removePropertyCache('query-value');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname]);
 
   return {
     query,
