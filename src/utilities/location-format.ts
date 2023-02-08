3cf79f454e717c10201f15e5d021ecde4a8a6cef
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-02-08 20:22:57 -0300

    Fix setting logic with the initial path

--- src/utilities/location-format.ts
+++ src/utilities/location-format.ts
@@ -3,10 +3,13 @@ import { Location } from 'react-router-dom';
 export const formatLocation = (location: Location) => {
   const pathnames = location.pathname.split('/');
   let currenPathname;
-  if (pathnames[1] === 'search') {
+  if (location.pathname === '/' || pathnames[1] === 'home') {
+    currenPathname = 'home';
+  } else if (pathnames[1] === 'search') {
     currenPathname = 'search';
   } else {
-    currenPathname = pathnames[1] !== 'home' ? pathnames[2] : 'home';
+    const pathnameCategory = pathnames[2];
+    currenPathname = pathnameCategory;
   }
   return { currenPathname, querySearch: location.search.substring(3, location.search.length) };
 };
