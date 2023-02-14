629a41e3b7b0b1ab0c6914e953a1cd17d97b2052
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-02-14 09:19:49 -0300

    Add Welcome test

---
+++ src/test/pages/Home/components/Welcome/Welcome.test.tsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react';
+import { Welcome } from '../../../../../pages/Home/components/Welcome';
+import { renderWhitContext } from '../../../../utils';
+
+type Props = { cache: Record<string, string> };
+
+describe('Test in <Welcome />', () => {
+  function setup({ cache }: Props) {
+    renderWhitContext({
+      children: <Welcome />,
+      pathname: '/home',
+      cache,
+    });
+  }
+
+  it('should match snapshot', () => {
+    setup({ cache: {} });
+    expect(screen).toMatchSnapshot();
+  });
+  it('should render main content', () => {
+    setup({ cache: {} });
+    screen.getByText('Welcome.');
+    screen.getByText('Most recent top news.');
+    screen.findByTestId('modal');
+  });
+  it('should render correctly information if the cache contain any country', () => {
+    setup({ cache: { country: 'Argentina' } });
+    screen.getByText('Top news from Argentina.');
+    expect(screen.queryByTestId('modal')).not.toBeInTheDocument();
+  });
+});
