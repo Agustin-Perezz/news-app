8e3215a70414a972694dac63ba052963a7b701a2
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-02-16 15:55:48 -0300

    Clean component SocialMedia

--- src/components/ui/SocialMedia/SocialMedia.tsx
+++ src/components/ui/SocialMedia/SocialMedia.tsx
@@ -1,8 +1,5 @@
-import { Avatar } from '@mui/material';
 import React from 'react';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import { Avatar } from '@mui/material';
 import { dataMedia } from './data-media';
 import { CustomStack } from '../CustomStack';
 
@@ -16,21 +13,11 @@ export const SocialMedia: React.FC = () => {
         margin: '0',
       }}
     >
-      <a href={dataMedia[0].url} target="_black">
-        <Avatar {...dataMedia[0]}>
-          <GitHubIcon />
-        </Avatar>
-      </a>
-      <a href={dataMedia[1].url} target="_black">
-        <Avatar {...dataMedia[1]}>
-          <LinkedInIcon />
-        </Avatar>
-      </a>
-      <a href={dataMedia[2].url} target="_black">
-        <Avatar {...dataMedia[2]}>
-          <TwitterIcon />
-        </Avatar>
-      </a>
+      {dataMedia.map((media) => (
+        <a href={media.url} target="_black" key={media.alt}>
+          <Avatar {...media}>{media.icon}</Avatar>
+        </a>
+      ))}
     </CustomStack>
   );
 };
--- src/components/ui/SocialMedia/data-media.tsx
+++ src/components/ui/SocialMedia/data-media.tsx
@@ -1,45 +1,42 @@
 import { SxProps } from '@mui/material';
 import { Theme } from '@mui/system';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 type MediaProps = {
   variant?: 'circular' | 'square' | 'rounded';
   alt?: string;
   url: string;
   sx?: SxProps<Theme>;
-  test?: JSX.Element;
+  icon: JSX.Element;
+};
+
+const sx = {
+  marginRight: 1,
+  '&:hover': { backgroundColor: 'rgb(170, 170, 170, 210)' },
 };
 
 export const dataMedia: MediaProps[] = [
   {
     alt: 'github-icon',
     variant: 'circular',
+    icon: <GitHubIcon />,
     url: 'https://github.com/Agustin-Perezz',
-    sx: {
-      marginRight: 1,
-      '&:hover': {
-        backgroundColor: 'rgb(170, 170, 170, 210)',
-      },
-    },
+    sx,
   },
   {
     alt: 'linkedin-icon',
     variant: 'circular',
+    icon: <LinkedInIcon />,
     url: 'https://www.linkedin.com/in/agustinperez-front-end-developer/',
-    sx: {
-      marginRight: 1,
-      '&:hover': {
-        backgroundColor: 'rgb(170, 170, 170, 210)',
-      },
-    },
+    sx,
   },
   {
     alt: 'twitter-icon',
     variant: 'circular',
+    icon: <TwitterIcon />,
     url: 'https://twitter.com/agustinperez__',
-    sx: {
-      '&:hover': {
-        backgroundColor: 'rgb(170, 170, 170, 210)',
-      },
-    },
+    sx,
   },
 ];
