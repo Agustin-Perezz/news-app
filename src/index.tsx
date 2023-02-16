624ee36568bf763cbbe3ec785d9bca84c62f0382
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-02-16 17:12:56 -0300

    Move providers to NewsApp component

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CacheProvider } from './context/CacheContext';
-import { NewsProvider } from './context/NewsContext';
 import { NewsApp } from './NewsApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <CacheProvider>
-      <NewsProvider>
-        <NewsApp />
-      </NewsProvider>
-    </CacheProvider>
+    <NewsApp />
   </React.StrictMode>
 );
