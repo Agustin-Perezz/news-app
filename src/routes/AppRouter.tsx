7aaaa9d39f3db9aee0be364812eca4c3f18b86d3
Agustin-Perezz <agustinnicolas806@gmail.com>
2023-02-16 20:06:43 -0300

    Implement Home and News routes with lazy loading

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,18 +1,22 @@
+import { Suspense } from 'react';
+
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { HeaderPage } from '../pages/Header';
 import { routes } from './router';
 
 const AppRouter: React.FC = () => {
   return (
-    <BrowserRouter basename="/news-app">
-      <HeaderPage />
-      <Routes>
-        {routes.map(({ path, Component }) => (
-          <Route key={path} path={path} element={<Component />} />
-        ))}
-        <Route path="/*" element={<Navigate replace to="/home" />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense>
+      <BrowserRouter basename="/news-app">
+        <HeaderPage />
+        <Routes>
+          {routes.map(({ path, Component }) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
+          <Route path="/*" element={<Navigate replace to="/home" />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
